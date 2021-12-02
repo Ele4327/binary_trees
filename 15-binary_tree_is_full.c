b86a694b961ebbe734b3033eba398c7fb312bc5c
yazgiraldoa <edygiraldo.97@gmail.com>
2021-12-02 15:28:38 -0500

    Full Tree

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
-* binary_tree_balance- function that measures balance factor of binary tree
-* @tree: pointer to the root node of the tree to measure the balance factor
-* Return: balance factor or 0 if tree is NULL.
+* binary_tree_is_full - function that checks if a binary tree is full
+* @tree: pointer to the root node of the tree to check.
+* Return: 1 if is full, 0 if not or also 0 if tree is NULL.
 */
 
 int binary_tree_is_full(const binary_tree_t *tree)
@@ -16,6 +16,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree->right && !tree->left)
 		return (1);
 
-	return ((binary_tree_is_full(tree->left) == 1) && (binary_tree_is_full (tree->right) == 1) ? 1 : 0);
-
+	return ((binary_tree_is_full(tree->left) == 1) &&
+	(binary_tree_is_full(tree->right) == 1) ? 1 : 0);
 }
